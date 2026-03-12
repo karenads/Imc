7c7f7d595ef2ad9cc69f8bbea7b39f3bed478700
karenads <karenrabelomoreira@gmail.com>
2026-03-11 22:28:08 -0300

    Add files via upload

---
+++ api-imc/src/main/java/com/example/api_imc/ApiImcApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_imc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiImcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiImcApplication.class, args);
+	}
+
+}
